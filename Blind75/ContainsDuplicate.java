c381d2327b11e7281f6ed881e9b576ea4d103485
Yoga ranjan singh <yoga.ranjan.singh@gmail.com>
2025-11-01 09:43:23 +0530

    Create ContainsDuplicate.java

---
+++ Blind75/ContainsDuplicate.java
@@ -0,0 +1,11 @@
+class ContainsDuplicate {
+    public boolean hasDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++) {
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+    return false;
+    }
+}
